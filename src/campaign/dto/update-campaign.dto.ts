bdc750b558ccaca5f2fbf800504291ec87223e54
huseyincanbay <huseyincanbay10@gmail.com>
2023-09-28 22:19:09 +0300

    updated update-dto parts of campaign and order

--- src/campaign/dto/update-campaign.dto.ts
+++ src/campaign/dto/update-campaign.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CampaignDto } from './create-campaign.dto';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateCampaignDto extends PartialType(CampaignDto) {}
+export class UpdateCampaignDto extends PartialType(CampaignDto) {
+  @IsString()
+  name?: string;
+
+  @IsNumber({}, { each: true })
+  orders?: number[];
+}
--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { OrderDto } from './create-order.dto';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
 
-export class UpdateOrderDto extends PartialType(OrderDto) {}
+export class UpdateOrderDto extends PartialType(OrderDto) {
+  @IsString()
+  customerName?: string;
+
+  @IsString()
+  product?: string;
+
+  @IsNumber()
+  @IsPositive()
+  price?: number;
+
+  @IsNumber()
+  campaignId?: number;
+}
