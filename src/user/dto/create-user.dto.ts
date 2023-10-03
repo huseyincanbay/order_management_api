d144995aaae5af2697627b1117e217276fb090b0
huseyincanbay <huseyincanbay10@gmail.com>
2023-10-04 00:27:40 +0300

    user dto and entity added

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, IsNumber, IsEmail } from 'class-validator';
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  readonly password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+}
